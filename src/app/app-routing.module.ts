15748a728fc8c774ddd47982d5a7c4c21f81fbff
Pali002 <zentaipali@gmail.com>
2023-03-07 14:58:56 +0100

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { GithubComponent } from './github/github.component';
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "about", component: AboutComponent},
+  {path: "students", component: StudentsComponent},
+  {path: "github", component: GithubComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
